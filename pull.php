cbe494d8a189fafbf7a7fe232693de5c696ae068
kuba <Jakub Troczynski>
2023-05-14 12:59:05 +0200

    com

---
+++ pull.php
@@ -0,0 +1,18 @@
+<?php
+
+// Use in the “Post-Receive URLs” section of your GitHub repo.
+//hellofff
+
+
+
+if ( $_POST['payload'] ) {
+    putenv('PATH=/usr/local/bin');
+
+
+    echo shell_exec('cd /var/www/woo && /usr/bin/git pull origin main 2>&1');
+
+    
+    }
+
+//echoghgfffff
+?>
