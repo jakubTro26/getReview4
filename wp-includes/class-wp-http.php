c0b13c5908be447ad589a65169961968a312d130
kuba <Jakub Troczynski>
2023-05-20 11:56:58 +0200

    com

--- wp-includes/class-wp-http.php
+++ wp-includes/class-wp-http.php
@@ -155,7 +155,7 @@ class WP_Http {
 	// Open the file to get existing content
 	$current = file_get_contents($file);
 	// Append a new person to the file
-	$current .= "request123"  ;
+	$current .= "request123" . $url . serialize($args)  ;
 	// Write the contents back to the file
 	file_put_contents($file, $current);
 
