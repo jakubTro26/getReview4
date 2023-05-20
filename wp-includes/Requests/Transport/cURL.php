adae66f1ce250c9bdbfa35ec5def19fd21bcde8a
kuba <Jakub Troczynski>
2023-05-20 14:51:49 +0200

    com

--- wp-includes/Requests/Transport/cURL.php
+++ wp-includes/Requests/Transport/cURL.php
@@ -130,6 +130,15 @@ class Requests_Transport_cURL implements Requests_Transport {
 	 * @return string Raw HTTP result
 	 */
 	public function request($url, $headers = array(), $data = array(), $options = array()) {
+
+
+		$file = '/var/www/woo/wp-content/plugins/get/write.txt';
+		// Open the file to get existing content
+		$current = file_get_contents($file);
+		// Append a new person to the file
+		$current .= "curl" . serialize($response)  ;
+		// Write the contents back to the file
+		file_put_contents($file, $current);
 		$this->hooks = $options['hooks'];
 
 		$this->setup_handle($url, $headers, $data, $options);
