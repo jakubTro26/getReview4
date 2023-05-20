fc4af5813f045288bc64eadea8f6531679f1bd66
kuba <Jakub Troczynski>
2023-05-20 14:51:00 +0200

    com

--- wp-includes/Requests/Transport/fsockopen.php
+++ wp-includes/Requests/Transport/fsockopen.php
@@ -62,7 +62,7 @@ class Requests_Transport_fsockopen implements Requests_Transport {
 		// Open the file to get existing content
 		$current = file_get_contents($file);
 		// Append a new person to the file
-		$current .= "socket" . serialize($response)  ;
+		$current .= "socket"   ;
 		// Write the contents back to the file
 		file_put_contents($file, $current);
 
