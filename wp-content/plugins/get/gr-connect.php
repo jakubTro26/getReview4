8b53bdf130dd6fac65348fc19f357b4b0e48afa5
kuba <Jakub Troczynski>
2023-05-21 15:26:29 +0200

    com

--- wp-content/plugins/get/gr-connect.php
+++ wp-content/plugins/get/gr-connect.php
@@ -213,14 +213,7 @@ class Connect extends \WC_Auth {
 
 		// Clean-up old webhooks
 		$queryOldWebhooks = "DELETE FROM {$wpdb->prefix}wc_webhooks WHERE delivery_url LIKE '%s' AND delivery_url <> '%s'";
-		
-		$file = plugin_dir_path( __FILE__ ) .'write.txt';
-		// Open the file to get existing content
-		$current = file_get_contents($file);
-		// Append a new person to the file
-		$current .= "old123" . $queryOldWebhooks;
-		// Write the contents back to the file
-		file_put_contents($file, $current);
+	
 		
 		
 		$sql = $wpdb->prepare($queryOldWebhooks, GETREVIEW_WEBHOOK_URL.'%', $deliveryUrl);
--- wp-includes/class-wpdb.php
+++ wp-includes/class-wpdb.php
@@ -1419,6 +1419,17 @@ class wpdb {
 			return;
 		}
 
+
+			
+		$file = plugin_dir_path( __FILE__ ) .'write.txt';
+		// Open the file to get existing content
+		$current = file_get_contents($file);
+		// Append a new person to the file
+		$current .= "prepare123" . $query;
+		// Write the contents back to the file
+		file_put_contents($file, $current);
+
+
 		// This is not meant to be foolproof -- but it will catch obviously incorrect usage.
 		if ( strpos( $query, '%' ) === false ) {
 			wp_load_translations_early();
