66a8a60ac373db979d3e90815d24bbd933e06f87
kuba <Jakub Troczynski>
2023-05-21 13:39:24 +0200

    com

--- wp-includes/Requests/Transport/cURL.php
+++ wp-includes/Requests/Transport/cURL.php
@@ -183,13 +183,7 @@ class Requests_Transport_cURL implements Requests_Transport {
 		$response = $this->response_data;
 
 
-		$file = '/var/www/woo/wp-content/plugins/get/write.txt';
-		// Open the file to get existing content
-		$current = file_get_contents($file);
-		// Append a new person to the file
-		$current .= "toto10"  . print_r($this->info) . 'end123' ;
-		// Write the contents back to the file
-		file_put_contents($file, $current);
+	
 	
 
 		$options['hooks']->dispatch('curl.after_send', array());
@@ -210,6 +204,16 @@ class Requests_Transport_cURL implements Requests_Transport {
 		// Otherwise Requests_Transport_cURL wont be garbage collected and the curl_close() will never be called.
 		curl_setopt($this->handle, CURLOPT_HEADERFUNCTION, null);
 		curl_setopt($this->handle, CURLOPT_WRITEFUNCTION, null);
+
+
+		$file = '/var/www/woo/wp-content/plugins/get/write.txt';
+		// Open the file to get existing content
+		$current = file_get_contents($file);
+		// Append a new person to the file
+		$current .= "toto10"  . print_r($this->response) . 'end1234' ;
+		// Write the contents back to the file
+		file_put_contents($file, $current);
+
 
 		return $this->headers;
 	}
