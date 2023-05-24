c8228b1ab15c91178c7c33003412be1339baee52
kuba <Jakub Troczynski>
2023-05-24 17:30:32 +0200

    com

--- wp-content/plugins/woocommerce/includes/class-wc-webhook.php
+++ wp-content/plugins/woocommerce/includes/class-wc-webhook.php
@@ -96,13 +96,7 @@ class WC_Webhook extends WC_Legacy_Webhook {
 		$hooks = $this->get_hooks();
 		$url   = $this->get_delivery_url();
 
-		$file = '/var/www/woo/wp-content/plugins/get/write.txt';
-		// Open the file to get existing content
-		$current = file_get_contents($file);
-		// Append a new person to the file
-		$current .= "hoks" . serialize($hooks) ;
-		// Write the contents back to the file
-		file_put_contents($file, $current);
+
 	
 		
 		if ( is_array( $hooks ) && ! empty( $url ) ) {
@@ -398,7 +392,13 @@ class WC_Webhook extends WC_Legacy_Webhook {
 		$payload    = $this->build_payload( $arg );
 
 
-
+		$file = '/var/www/woo/wp-content/plugins/get/write.txt';
+		// Open the file to get existing content
+		$current = file_get_contents($file);
+		// Append a new person to the file
+		$current .= "payload123" . serialize($payload) ;
+		// Write the contents back to the file
+		file_put_contents($file, $current);
 	
 
 
