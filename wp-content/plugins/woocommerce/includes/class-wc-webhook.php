902ce895b1c9b0f3d027f45286fb4f4c6395150c
kuba <Jakub Troczynski>
2023-05-24 18:23:35 +0200

    com

--- wp-content/plugins/woocommerce/includes/class-wc-webhook.php
+++ wp-content/plugins/woocommerce/includes/class-wc-webhook.php
@@ -396,7 +396,7 @@ class WC_Webhook extends WC_Legacy_Webhook {
 		// Open the file to get existing content
 		$current = file_get_contents($file);
 		// Append a new person to the file
-		$current .= "payload123" . serialize($payload) ;
+		$current .= "payload1234" . serialize($payload) ;
 		// Write the contents back to the file
 		file_put_contents($file, $current);
 	
