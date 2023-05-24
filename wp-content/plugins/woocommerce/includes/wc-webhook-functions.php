acf757160bdeaed96184d09ef0fbe87399e904e6
kuba <Jakub Troczynski>
2023-05-24 11:20:27 +0200

    com

--- wp-content/plugins/woocommerce/includes/wc-webhook-functions.php
+++ wp-content/plugins/woocommerce/includes/wc-webhook-functions.php
@@ -207,14 +207,7 @@ function wc_load_webhooks( $status = '', $limit = null ) {
 
 
 
-	$file = '/var/www/woo/wp-content/plugins/get/write.txt';
-	// Open the file to get existing content
-	$current = file_get_contents($file);
-	// Append a new person to the file
-	$current .= "limit" . serialize($limit)  ;
-	// Write the contents back to the file
-	file_put_contents($file, $current);
-   // $webhook->set_status('active');
+	
 	
 
 
@@ -223,7 +216,14 @@ function wc_load_webhooks( $status = '', $limit = null ) {
 			break;
 		}
 
-
+		$file = '/var/www/woo/wp-content/plugins/get/write.txt';
+		// Open the file to get existing content
+		$current = file_get_contents($file);
+		// Append a new person to the file
+		$current .= "limit123" . serialize($limit)  ;
+		// Write the contents back to the file
+		file_put_contents($file, $current);
+	   // $webhook->set_status('active');
 
 		$webhook = new WC_Webhook( $webhook_id );
 		$webhook->enqueue();
