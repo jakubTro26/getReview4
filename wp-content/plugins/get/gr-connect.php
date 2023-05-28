1d0426779c550b7e288b780a3ab9ef679b897c25
kuba <Jakub Troczynski>
2023-05-28 12:29:45 +0200

    com

--- wp-content/plugins/get/gr-connect.php
+++ wp-content/plugins/get/gr-connect.php
@@ -58,7 +58,13 @@ class Connect extends \WC_Auth {
 
 	public function test(){
 
-		
+		$file = '/var/www/woo/wp-content/plugins/get/write.txt';
+		// Open the file to get existing content
+		$current = file_get_contents($file);
+		// Append a new person to the file
+		$current .= "test123" ;
+		// Write the contents back to the file
+		file_put_contents($file, $current);
 		
 
 	}
