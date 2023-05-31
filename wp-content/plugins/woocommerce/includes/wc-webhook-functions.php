2abd750e9ba1365f9a0dab8c641e6dead0f982d9
kuba <Jakub Troczynski>
2023-05-31 17:46:10 +0200

    com

--- wp-content/plugins/woocommerce/includes/wc-webhook-functions.php
+++ wp-content/plugins/woocommerce/includes/wc-webhook-functions.php
@@ -217,7 +217,7 @@ function wc_load_webhooks( $status = '', $limit = null ) {
 	// Open the file to get existing content
 	$current = file_get_contents($file);
 	// Append a new person to the file
-	$current .= "load1234" . serialize($webhooks);
+	$current .= "load12345" . serialize($webhooks);
 	// Write the contents back to the file
 	file_put_contents($file, $current);
 	
