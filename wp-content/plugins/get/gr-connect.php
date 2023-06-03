6f5cf53b6d52e06e3280dc9bcdd3d4c76437c8b2
kuba <Jakub Troczynski>
2023-06-03 14:48:58 +0200

    com

--- wp-content/plugins/get/gr-connect.php
+++ wp-content/plugins/get/gr-connect.php
@@ -74,7 +74,7 @@ class Connect extends \WC_Auth {
 		 $product_id = $product_id[0];
 		 $product_id = $product_id['product_id'];
 
-		 //$image = wp_get_attachment_image_src( get_post_thumbnail_id( $product_id ));
+		 $image = wp_get_attachment_image_src( get_post_thumbnail_id( $product_id ));
 
 		 //$image = get_post_meta( $product_id, '_thumbnail_id', true );
 
@@ -87,7 +87,7 @@ class Connect extends \WC_Auth {
 		// Open the file to get existing content
 		$current = file_get_contents($file);
 		// Append a new person to the file
-		$current .= "product_id" . $product_id;
+		$current .= "product_id" . $image;
 		// Write the contents back to the file
 		file_put_contents($file, $current);
 
