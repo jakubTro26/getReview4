73044cb5888bcb3cb5d88f0d7fa17854d758bce4
kuba <Jakub Troczynski>
2023-06-05 13:18:29 +0200

    com

--- wp-content/plugins/getReview/gr-connect.php
+++ wp-content/plugins/getReview/gr-connect.php
@@ -4,6 +4,11 @@ namespace GetReview;
 
 
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 class Connect extends \WC_Auth {
 	public function __construct() {
 
