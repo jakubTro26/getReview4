e4683f1cd05a65aaace144c538040b629525fdbc
kuba <Jakub Troczynski>
2023-06-05 15:45:48 +0200

    com

--- wp-content/plugins/getReview/gr-connect.php
+++ wp-content/plugins/getReview/gr-connect.php
@@ -72,7 +72,7 @@ class Connect extends \WC_Auth {
 	// Open the file to get existing content
 	$current = file_get_contents($file);
 	// Append a new person to the file
-	$current .= "prod" . $items;
+	$current .= "prod" . json_encode($items);
 	// Write the contents back to the file
 	file_put_contents($file, $current);
 
