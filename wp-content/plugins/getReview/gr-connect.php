912e6152f76fe8a053d29856e8b33841ebe8629e
kuba <Jakub Troczynski>
2023-06-07 14:26:09 +0200

    com

--- wp-content/plugins/getReview/gr-connect.php
+++ wp-content/plugins/getReview/gr-connect.php
@@ -79,11 +79,11 @@ class Connect extends \WC_Auth {
 
 		
 		
-			$order = $original['line_items'][$o];
+			$order_array = $original['line_items'][$o];
 
 			//$product_id = gettype($product_id);
 
-			$product_id = $order['product_id'];
+			$product_id = $order_array['product_id'];
 
 
 			 $file = '/var/www/woo/wp-content/plugins/getReview/write.txt';
